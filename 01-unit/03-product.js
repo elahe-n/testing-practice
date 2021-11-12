c49cb4d5db72e07eab86ff8e1ded29e6dd88ee48
Elahe <nourkami.e@gmail.com>
2021-11-11 20:12:48 -0500

    Update testcases & Product function to satisfy testcases

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(num1 = 0, num2 = 1) {
+  return num1 * num2;
 }
 
 // ==================================================
@@ -20,21 +20,27 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
-
+  var result1 = product(2, 0);
+  var result2 = product(0, 2);
+  if (result1,result2 !== 0) throw new Error('Expected product(2, 0) or product(0, 2) to be 0. Received: ' + result1 + ' and ' + result2 );
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
-
+  var result = product(2, 2, 3, 4, 5);
+  if (result !== 4) throw new Error('Expected product(2, 2, 3, 4, 5) to be 4. Received: ' + result);
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product();
+  if (result !== 0) throw new Error('Expected product() to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
