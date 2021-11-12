a2b08486f49b90eac79b3a7dd64ed490fc5d63b9
Elahe <nourkami.e@gmail.com>
2021-11-11 20:42:44 -0500

    Update testcases & quotient function to satisfy testcases

--- 01-unit/04-quotient.js
+++ 01-unit/04-quotient.js
@@ -1,8 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function quotient() {
-
+function quotient(num1 = 0, num2 = 1) {
+  if (num2 == 0) {
+    return'ERROR'
+  }
+  else
+  return num1 / num2;
 }
 
 // ==================================================
@@ -20,22 +24,27 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the text 'ERROR' when the second number is 0.
-
+  var result = quotient(2, 0);
+  if (result !== 'ERROR') throw new Error('Expected quotient(2, 0) to be ERROR Received: ' + result);
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = quotient(2, 2, 3, 4, 5);
+  if (result !== 1) throw new Error('Expected quotient(2, 2, 3, 4, 5) to be 1. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-
+  var result = quotient(2);
+  if (result !== 2) throw new Error('Expected quotient(2) to be 2. Received: ' + result);
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-
+  var result = quotient();
+  if (result !== 0) throw new Error('Expected quotient() to be 0. Received: ' + result);
   console.log('All tests passed successfully.');
 
 // ==================================================
